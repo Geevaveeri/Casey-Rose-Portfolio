376ed69409ee8a123a4edc2a572695a2fb9733a4
Geevaveeri <59940979+Geevaveeri@users.noreply.github.com>
2021-06-14 18:22:40 -0600

    made slide text more readable and changed color pallete

--- js/init.js
+++ js/init.js
@@ -1,18 +1,10 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
+(function ($) {
+  $(function () {
+    $(".sidenav").sidenav();
+    $(".parallax").parallax();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-document.addEventListener("DOMContentLoaded", function () {
-  var elems = document.querySelectorAll(".slider");
-  var instances = M.Slider.init(elems);
-});
-
-// Or with jQuery
 
 $(document).ready(function () {
   $(".slider").slider();
